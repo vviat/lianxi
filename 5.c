ae488026b15836d04ba3312989c694195cf3b0fc
ygy <40139861+vviat@users.noreply.github.com>
2018-07-09 21:30:43 +0800

    switch语句

---
+++ 5.c
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: 5.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年04月04日 星期三 18时27分16秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main(){
+int n;
+scanf("%d",&n);
+switch(n){
+    case 1:printf("one");
+    case 2:printf("two");
+    case 3:printf("three");
+    default :printf("error");
+}
+    return 0;
+}
